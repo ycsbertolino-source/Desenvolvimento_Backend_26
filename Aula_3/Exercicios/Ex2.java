c63c5f3e5555c9eb407d8e41970c492c03e29ec8
ycsbertolino <ycbertolino@gmail.com>
2026-08-17 22:44:34 -0300

    Adiciona exercício Ex2 verificação maioridade

--- Aula_3/Exercicios/Ex2.java
+++ Aula_3/Exercicios/Ex2.java
@@ -1,4 +1,26 @@
 package Aula_3.Exercicios;
+
+import java.util.Scanner;
+
+//Criando  Verificação de maioridade
 public class Ex2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita a idade ao usuário
+        System.out.print("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        // Lógica de verificação
+        if (idade >= 18) {
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Menor de idade");
+        }
+
+        scanner.close();
+    }
+
     
+
 }
